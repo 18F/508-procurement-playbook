7d1a2594d84e39e358f1e1e45370d2b502c69b2e
Alan deLevie <adelevie@gmail.com>
2015-04-01 11:14:48 -0400

    added an ampersand

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,3 +1,7 @@
-nohup http-server -p 8080
-pa11y -r csv localhost:8080/index.html -> pa11y.csv
+portslay () { 
+  kill -9 `lsof -i tcp:$1 | tail -1 | awk '{ print $2;}'` 
+}
+
+nohup http-server -p 8080 &
+pa11y -r csv localhost:8080/index.html > pa11y.csv
 cat pa11y.csv
