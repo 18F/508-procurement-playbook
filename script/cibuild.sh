af24d3976864a7c51eb938f34fb6a76f86032e9f
Alan deLevie <adelevie@gmail.com>
2015-04-01 12:18:13 -0400

    Functioning pa11y tester

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,7 +1,52 @@
-portslay () { 
-  kill -9 `lsof -i tcp:$1 | tail -1 | awk '{ print $2;}'` 
+MAX_ERRORS=0
+MAX_WARNINGS=10
+MAX_NOTICES=1000
+STANDARD="WCAG2AAA"
+
+nohup http-server -p 8080 >/dev/null 2>&1 &
+pa11y -s $STANDARD -r json localhost:8080/index.html > pa11y.json
+
+function count_type () {
+  cat pa11y.json | json count.$1
 }
 
-nohup http-server -p 8080 &
-pa11y -r csv localhost:8080/index.html > pa11y.csv
-cat pa11y.csv
+error_count=`count_type "error"`
+warning_count=`count_type "warning"`
+notice_count=`count_type "notice"`
+
+fail=false
+
+if [ "$error_count" -le "$MAX_ERRORS" ]
+then
+  echo "$STANDARD errors passed"
+else
+  echo "$STANDARD errors failed: expected $MAX_ERRORS errors, got $error_count"
+  fail=true
+fi
+
+if [ "$warning_count" -le "$MAX_WARNINGS" ]
+then
+  echo "$STANDARD warnings passed"
+else
+  echo "$STANDARD warnings failed: expected $MAX_WARNINGS warnings, got $warning_count"
+  fail=true
+fi
+
+if [ "$notice_count" -le "$MAX_NOTICES" ]
+then
+  echo "$STANDARD notices passed"
+else
+  echo "$STANDARD notices failed: expected $MAX_NOTICES notices, got $notice_count"
+  fail=true
+fi
+
+echo "See the pa11y report for details:"
+cat pa11y.json | json
+
+if [ $fail == true ]
+then
+  exit 1
+fi
+
+rm pa11y.json
+
